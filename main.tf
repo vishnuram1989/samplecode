e1e2fb92223730228960fdeb677669d524978e34
Vishnu Ram PG <vishnurampg@hotmail.com>
2021-05-04 22:07:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,18 @@
 resource "null_resource" "check_name" {
 
 }
+
+
+data "google_dns_managed_zone" "env_dns_zone" {
+  name = "steven-zone"
+}
+
+resource "google_dns_record_set" "dns" {
+  name = "my-address.${data.google_dns_managed_zone.env_dns_zone.dns_name}"
+  type = "TXT"
+  ttl  = 300
+
+  managed_zone = data.google_dns_managed_zone.env_dns_zone.name
+
+  rrdatas = ["test"]
+}
